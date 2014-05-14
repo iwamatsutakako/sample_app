bd09fde1ce289b6dbb6404bc5bce633b448628c7
takako <takakoiwamatsu.sc@gmail.com>
2014-05-14 17:40:39 +0900

    Finish static pages

--- static_pages_spec.rb
+++ static_pages_spec.rb
@@ -6,7 +6,15 @@ describe "Static pages" do
 
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
+      page.should have_content('Sample App')
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_content('Help')
     end
   end
 end
